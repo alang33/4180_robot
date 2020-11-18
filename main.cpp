1111ba4c16d16bb042c52945a9a95ee7cf385f1e
whamil3 <72262341+whamil3@users.noreply.github.com>
2020-11-17 22:10:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include "USBSerial.h"
 #include "uLCD_4DGL.h"
 #include <string>
-#include <cstdlib>
 #define NULL 0
 
-using namespace std;
 
 uLCD_4DGL LCD(p28, p27, p30); // create a global uLCD object
 //Pi mbed USB Slave function
@@ -14,31 +12,26 @@ RawSerial  pc(USBTX, USBRX);
 USBSerial pi;
 
 
-
-
 int main(void) {
     uint8_t buf[128];
-    string buf_text;
+    std::string buf_text;
     char time_buffer[32];
     
     pi.scanf("%s", buf); //read serial data from Pi
     pi.printf("recv: %s\n", buf); //Print data back to terminal
     pc.printf("recv: %s\r\n", buf); //Print data to PC terminal
     buf_text = (const char*)buf; //convert buffer to string
-    int buf_int = std::atoi(buf_text.c_str()); //convert string to int
-       
-    pc.printf("Bar: %i\n", buf_int); //print initialized buf int
+    int buf_int = std::atoi(buf_text.c_str()); //convert string to int 
+    //pc.printf("Seconds: %i\n", buf_int); //print initialized buf int
     set_time(buf_int); //set the clock using seconds since 1970
     LCD.color(RED); //1605658168
     while (true) {
         time_t seconds = time(NULL); //start RTC
-        //pc.printf("Time as seconds since January 1, 1970 = %u\n", (unsigned int)seconds);
         strftime(time_buffer, 32, "%I:%M %p\n", localtime(&seconds));
         LCD.locate(5,5);
-        //LCD.printf("%s", ctime(&seconds));
         LCD.printf("%s", time_buffer); //print current time to LCD
-        pc.printf("%s", ctime(&seconds)); //print full date to PC  
-        //pc.printf("Time as a custom formatted string = %s", time_buffer);
+        //LCD.printf("%s", ctime(&seconds));
+        //pc.printf("%s\r\n", ctime(&seconds)); //print full date to PC  
         wait(1);
     }
 }
