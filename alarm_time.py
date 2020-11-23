27e4fbf0da23fa47acee3c8198fe309eb7af3c32
whamil3 <72262341+whamil3@users.noreply.github.com>
2020-11-23 00:37:14 -0500

    Delete alarm_time.py

--- alarm_time.py
+++
@@ -1,9 +0,0 @@
-import serial
-import os
-
-fs = open("/var/www/html/alarm.txt", "r") 
-hour = fs.read(2)
-minute = fs.read(2)
-fs.close()
-command = "echo " + hour + minute + " > /dev/ttyACM0"
-os.system(command)
